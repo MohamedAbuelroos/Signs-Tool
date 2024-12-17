fcf31c95647d889d53732834604f9e446351667d
mohamedrabea <mohamedabuelroos27@gmail.com>
2024-12-18 00:23:30 +0200

    Change-user-pass

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,6 +1,6 @@
 // src/data/users.ts
 export const users = [
   { name: "Mohamed", code: "12345", role: "Admin" },
-  { name: "Ramadan", code: "1234", role: "User" },
+  { name: "Ramadan", code: "2532801", role: "User" },
   { name: "Ahmed", code: "5678", role: "User" },
 ];
